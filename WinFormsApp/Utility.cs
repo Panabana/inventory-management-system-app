74be17e74bd83f927a5c7dcb53950616d99e2983
Alex <palexanderbengtsson@gmail.com>
2023-01-26 13:53:31 +0000

    Added Utility class + label success/failure msg methods

---
+++ WinFormsApp/Utility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp
+{
+    public class Utility
+    {
+        //Input which label you want to change the text for. Successful messages are black.
+        public void labelMessageSuccess(Label chosenLabel,string labelInput)
+        {
+            chosenLabel.Text = (labelInput);
+            chosenLabel.ForeColor = Color.Black;
+            chosenLabel.Visible = true;
+        }
+
+        //Input which label you want to change the text for. Successful messages are red.
+        public void labelMessageFailure(Label chosenLabel, string labelInput)
+        {
+            chosenLabel.Text = (labelInput);
+            chosenLabel.ForeColor = Color.Red;
+            chosenLabel.Visible = true;
+        }
+    }
+}
