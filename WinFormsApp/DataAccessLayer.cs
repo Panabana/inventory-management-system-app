9a2a1faf894ec6a880f51e1e01a0859cd038881d
AxelAlv <ax4821al-s@student.lu.se>
2023-01-26 16:40:43 +0000

    Revert "Update DataAccessLayer.cs"
    This reverts commit 99a37df3fd0d719671e6c43857024cab40da4c66.

--- WinFormsApp/DataAccessLayer.cs
+++ WinFormsApp/DataAccessLayer.cs
@@ -22,7 +22,55 @@ namespace WinFormsApp
 
             return connection;
         }
-        
+//
+//
+        private string connectionString;
+
+        public DataAccessLayer(String connectionString)
+        {
+            this.connectionString = connectionString;
         }
-}
+
+        public DataSet GetCustomers()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+
+            {
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Customer", connection))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataSet customers = new DataSet();
+                        adapter.Fill(customers);
+                    }
+                }
+
+            }
+        }
+
+        public void UpdateCustomer (DataSet ds)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("UPDATE Customers SET CustomerName = @Name, CustomerAddress = @Address, PhoneNumber = @Phone" +
+                                                             " WHERE CustomerID = @ID", connection))
+                {
+                    command.Parameters.Add("@Name", SqlDbType.VarChar);
+                    command.Parameters.Add("@Address", SqlDbType.VarChar);
+                    command.Parameters.Add("@Phone", SqlDbType.Int);
+                    command.Parameters.Add("@ID", SqlDbType.Int);
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.UpdateCommand = command;
+                        adapter.Update(ds);
+                    }
+                }
+
+
+            }
+        }
+        
+           
+        }
+    }
 
