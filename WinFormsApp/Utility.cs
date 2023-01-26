498f445058bafcc523d325ba29219286d4dc5657
Alex <palexanderbengtsson@gmail.com>
2023-01-26 14:28:49 +0000

    Changed name

--- WinFormsApp/Utility.cs
+++ WinFormsApp/Utility.cs
@@ -9,17 +9,17 @@ namespace WinFormsApp
     public class Utility
     {
         //Input which label you want to change the text for. Successful messages are black.
-        public void labelMessageSuccess(Label chosenLabel,string labelInput)
+        public void labelMessageSuccess(Label chosenLabel,string textInput)
         {
-            chosenLabel.Text = (labelInput);
+            chosenLabel.Text = (textInput);
             chosenLabel.ForeColor = Color.Black;
             chosenLabel.Visible = true;
         }
 
         //Input which label you want to change the text for. Successful messages are red.
-        public void labelMessageFailure(Label chosenLabel, string labelInput)
+        public void labelMessageFailure(Label chosenLabel, string textInput)
         {
-            chosenLabel.Text = (labelInput);
+            chosenLabel.Text = (textInput);
             chosenLabel.ForeColor = Color.Red;
             chosenLabel.Visible = true;
         }
