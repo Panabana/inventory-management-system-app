1d73d3710c87b18818d290e021790b11405d25b3
AxelAlv <ax4821al-s@student.lu.se>
2023-02-13 10:53:24 +0000

    Error Handling of the primary key violation in customer

--- WinFormsApp/FormManageCustomers.cs
+++ WinFormsApp/FormManageCustomers.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -62,6 +63,16 @@ namespace WinFormsApp
             {
                 Utility.LabelMessageFailure(labelManageCustomersMessage, "Please enter the fields in the correct format");
             }
+
+            catch(SqlException ex)
+            {
+                if(ex.Number == 2627)
+                {
+                    Utility.LabelMessageFailure(labelManageCustomersMessage, "A customer with this ID already exists");
+                }
+            }
+
+            
 
 
             catch (Exception ex)
