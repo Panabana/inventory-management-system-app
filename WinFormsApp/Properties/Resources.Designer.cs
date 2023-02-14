567e98db64506bbc7bce810ab1abe71c1e36d57e
Joseph Nordin <43850387+alhethmey@users.noreply.github.com>
2023-02-14 20:32:29 +0000

    Remove Metro and added text to the BG

--- WinFormsApp/Properties/Resources.Designer.cs
+++ WinFormsApp/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace WinFormsApp.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap BG_Inventory_text {
+            get {
+                object obj = ResourceManager.GetObject("BG_Inventory_text", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
