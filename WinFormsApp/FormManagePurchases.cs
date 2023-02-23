1746b880d295b78a4348325020814a3a8200903d
lukyor <lukas.yorke@gmail.com>
2023-02-23 17:31:30 +0100

    Remove purchase based on entered ID or selected value from grid view

--- WinFormsApp/FormManagePurchases.cs
+++ WinFormsApp/FormManagePurchases.cs
@@ -1,4 +1,5 @@
-﻿using DevExpress.Utils.About;
+﻿using DevExpress.Mvvm.Native;
+using DevExpress.Utils.About;
 using DevExpress.XtraEditors;
 using System;
 using System.Collections.Generic;
@@ -157,9 +158,13 @@ namespace WinFormsApp
                     Utility.LabelMessageFailure(labelManagePurchasesMessage, "Please select the correct Customer and Employee");
                 }
             }
-            catch (Exception ex)
+            catch (FormatException)
             {
-                Utility.LabelMessageFailure(labelManagePurchasesMessage, ex.Message);
+                Utility.LabelMessageFailure(labelManagePurchasesMessage, "Please enter a valid ID!");
+            }
+            catch (Exception)
+            {
+                Utility.LabelMessageFailure(labelManagePurchasesMessage, "Something went wrong!");
             }
         }
 
@@ -190,24 +195,37 @@ namespace WinFormsApp
         {
             try
             {
-                int selectedRowIndex = dataGridViewPurchase.CurrentRow.Index; //med hjälp av chatGPT
-                int purchaseID = Convert.ToInt32(dataGridViewPurchase.Rows[selectedRowIndex].Cells["Purchase ID"].Value);
-                //Utility.ClearTextBoxes(this);
-
-                //ChatGPT
-                if (dataGridViewPurchase.SelectedRows.Count > 0)
+                if(textBoxPurchaseID.Text.IsEmptyOrSingle()) 
                 {
-                    dataGridViewPurchase.Rows.Remove(dataGridViewPurchase.SelectedRows[0]);
+                    int selectedRowIndex = dataGridViewPurchase.CurrentRow.Index; //med hjälp av chatGPT
+                    int purchaseID = Convert.ToInt32(dataGridViewPurchase.Rows[selectedRowIndex].Cells["Purchase ID"].Value);
+                    //Utility.ClearTextBoxes(this);
+
+                    //ChatGPT
+                    if (dataGridViewPurchase.SelectedRows.Count > 0)
+                    {
+                        dataGridViewPurchase.Rows.Remove(dataGridViewPurchase.SelectedRows[0]);
+                        _layer.DeletePurchase(purchaseID);
+                    }
+
+
+                    Utility.LabelMessageSuccess(labelManagePurchasesMessage, "Purchase deleted!");
+                }
+                else if (!textBoxPurchaseID.Text.IsEmptyOrSingle())
+                {
+                    int purchaseID = Convert.ToInt32(textBoxPurchaseID.Text);
                     _layer.DeletePurchase(purchaseID);
+                    Utility.LabelMessageSuccess(labelManagePurchasesMessage, "Purchase deleted!");
                 }
 
-                
-                Utility.LabelMessageSuccess(labelManagePurchasesMessage, "Purchase deleted!");
             }
-            catch (Exception ex)
+            catch (FormatException)
             {
-                Utility.LabelMessageFailure(labelManagePurchasesMessage, ex.Message);
-
+                Utility.LabelMessageFailure(labelManagePurchasesMessage, "Please enter a valid ID!");
+            }
+            catch (Exception)
+            {
+                Utility.LabelMessageFailure(labelManagePurchasesMessage, "Something went wrong!");
             }
         }
         
