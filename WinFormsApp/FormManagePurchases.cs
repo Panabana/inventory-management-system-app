49fdf069fbf9dda85612c6e13efe9dab1034ec3c
lukyor <lukas.yorke@gmail.com>
2023-02-26 16:36:54 +0100

    fixed error

--- WinFormsApp/FormManagePurchases.cs
+++ WinFormsApp/FormManagePurchases.cs
@@ -186,8 +186,8 @@ namespace WinFormsApp
             try
             {
                 int selectedRowIndex = dataGridViewPurchase.CurrentRow.Index; //med hjälp av chatGPT
-                int purchaseID = Convert.ToInt32(dataGridViewPurchase.Rows[selectedRowIndex].Cells["PurchaseID"].Value); //chatGPT
-                int productID = Convert.ToInt32(dataGridViewPurchase.Rows[selectedRowIndex].Cells["ProductID"].Value); //chatGPT
+                int purchaseID = Convert.ToInt32(dataGridViewPurchase.Rows[selectedRowIndex].Cells["Purchase ID"].Value); //chatGPT
+                int productID = Convert.ToInt32(dataGridViewPurchase.Rows[selectedRowIndex].Cells["Product ID"].Value); //chatGPT
 
                 // chatGPT
                 if(dataGridViewPurchase.SelectedRows.Count > 0)
